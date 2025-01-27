16a95a2f4b6589b1667aa4d87b9df90c7c0203f8
David Pilarski <david.pilarski@hotmail.com>
2025-01-27 18:12:59 -0500

    Update API URL for production environment

--- app/src/config/constants.ts
+++ app/src/config/constants.ts
@@ -1,4 +1,4 @@
-export const apiUrl = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:8080';
+export const apiUrl = process.env.NODE_ENV === 'production' ? 'https://api.gshub.pro' : 'http://localhost:8080';
 
 export const navigationItems = [
   { path: "containers", label: "Containers", icon: "TbBox" },
